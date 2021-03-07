c7f896c7c51362ba31d57b1d9a588616bf8673ec
Legrain Samuel <samuel.legrain@bstorm.be>
2021-03-07 23:55:29 +0100

    Ajoutez des fichiers projet.

---
+++ ASP.Model.Global/Models/AspUser.cs
@@ -0,0 +1,21 @@
+﻿using ASP.Model.Comon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.Model.Global.Models
+{
+    public class AspUser : IDataModel<int>
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string RegNational { get; set; }
+        public string Bio { get; set; }
+    }
+}
